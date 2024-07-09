8967503c9f1bc41de9ffb409ac13692aa73756bb
jacoobes <jngu195@lsu.edu>
2024-07-09 16:09:03 -0500

    more frames

--- src/jacoobes/cljgguf.clj
+++ src/jacoobes/cljgguf.clj
@@ -1,6 +1,6 @@
 (ns jacoobes.cljgguf
   (:require [gloss.io :as gio :refer [decode]])
-  (:require [gloss.core :as gcore :refer [compile-frame string]])
+  (:require [gloss.core :as gcore :refer [compile-frame string enum defcodec]])
   (:require [clojure.java.io :as io]))
 
 (defn slurp-bytes
@@ -16,11 +16,28 @@
                                   :version :int32-le
                                   :tensor_count :uint64-le 
                                   :metadata_kv_count :uint64-le)))
-(defn metadata-tensor-frame [mct tct] 
-   [
-    
-    ])
+(def gguf_string (compile-frame 
+                   (string :ascii :length 65535 :delimiters [ \0 ])))
 
+(defcodec tmetadata (enum :int32 GGUF_METADATA_VALUE_TYPE_UINT8
+                                 GGUF_METADATA_VALUE_TYPE_INT8
+                                 GGUF_METADATA_VALUE_TYPE_UINT16
+                                 GGUF_METADATA_VALUE_TYPE_INT16
+                                 GGUF_METADATA_VALUE_TYPE_UINT32
+                                 GGUF_METADATA_VALUE_TYPE_INT32
+                                 GGUF_METADATA_VALUE_TYPE_FLOAT32
+                                 GGUF_METADATA_VALUE_TYPE_BOOL
+                                 GGUF_METADATA_VALUE_TYPE_STRING
+                                 GGUF_METADATA_VALUE_TYPE_ARRAY
+                                 GGUF_METADATA_VALUE_TYPE_UINT64
+                                 GGUF_METADATA_VALUE_TYPE_INT64
+                                 GGUF_METADATA_VALUE_TYPE_FLOAT64))
+
+(defn metadata-frame [mct] 
+  
+  )
+
+(defn tensor-frame [tct] tct)
 
 (defn parse [x]
   (let [buf (slurp-bytes x)
