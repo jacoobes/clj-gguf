e6352ab46cb895d35c65c9d6f247d38ec12715f7
jacoobes <jngu195@lsu.edu>
2024-07-10 23:18:03 -0500

    fix gguf_str

--- src/jacoobes/cljgguf.clj
+++ src/jacoobes/cljgguf.clj
@@ -2,6 +2,8 @@
   (:require [gloss.io :as gio :refer [decode]])
   (:require [gloss.core :as gcore :refer [compile-frame 
                                           string 
+                                          prefix
+                                          finite-frame
                                           enum 
                                           defcodec
                                           header ]])
@@ -15,13 +17,9 @@
     (gio/to-byte-buffer (.toByteArray out))))
 
 
-(def gguf-header (compile-frame (gcore/ordered-map  
-                                  :gguf (string :ascii :length 4)  
-                                  :version :int32-le
-                                  :tensor_count :uint64-le 
-                                  :metadata_kv_count :uint64-le)))
-(def gguf_string (compile-frame 
-                   (string :ascii :length 65535 :delimiters [ \0 ])))
+
+(def gguf_string (finite-frame (prefix :uint64-le) 
+                               (string :ascii)))
 
 (defcodec gguf_metadata_t (enum :int32 
                                 :GGUF_METADATA_VALUE_TYPE_UINT8
@@ -52,7 +50,8 @@
 (defcodec GGUF_METADATA_VALUE_TYPE_INT64 {:type :GGUF_METADATA_VALUE_TYPE_INT64 })
 (defcodec GGUF_METADATA_VALUE_TYPE_FLOAT64 {:type :GGUF_METADATA_VALUE_TYPE_FLOAT64 })
 
-(defn metadata-frame [mct] 
+
+(def metadata_pair 
   (header gguf_metadata_t {:GGUF_METADATA_VALUE_TYPE_UINT8 GGUF_METADATA_VALUE_TYPE_UINT8 
                            :GGUF_METADATA_VALUE_TYPE_INT8 GGUF_METADATA_VALUE_TYPE_INT8 
                            :GGUF_METADATA_VALUE_TYPE_UINT16 GGUF_METADATA_VALUE_TYPE_UINT16 
@@ -65,17 +64,24 @@
                            :GGUF_METADATA_VALUE_TYPE_ARRAY GGUF_METADATA_VALUE_TYPE_ARRAY 
                            :GGUF_METADATA_VALUE_TYPE_UINT64 GGUF_METADATA_VALUE_TYPE_UINT64 
                            :GGUF_METADATA_VALUE_TYPE_INT64 GGUF_METADATA_VALUE_TYPE_INT64 
-                           :GGUF_METADATA_VALUE_TYPE_FLOAT64 GGUF_METADATA_VALUE_TYPE_FLOAT64 } :type)
-  )
+                           :GGUF_METADATA_VALUE_TYPE_FLOAT64 GGUF_METADATA_VALUE_TYPE_FLOAT64 } :type))
 
-(defn tensor-frame [tct] tct)
+
+(def gguf-header (compile-frame (gcore/ordered-map :gguf (string :ascii :length 4)  
+                                                   :version :int32-le
+                                                   :tensor_count :uint64-le 
+                                                   :metadata_kv_count :uint64-le)))
+
+(defn metadata-frame [metadatact] )
+
+(defn tensor-frame [tensorct] tct)
 
 (defn parse [x]
   (let [buf (slurp-bytes x)
         header (decode gguf-header buf false)]
     ; after reading headers, we should be at 24 bytes
     (do (.position buf 24)
-        (let [ ])
+        ; to fix
         )))
 
 
